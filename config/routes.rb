9f432347894877c1e2d1a102bccce7277a5733b4
paskuvan <majopaskuvan@gmail.com>
2018-07-27 21:00:07 -0400

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  get 'companies/index'
   resources :areas
-  resources :companies
+  resources :companies do
+    resources :employees, only: [:create]
+  end
+
+  root 'companies#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
